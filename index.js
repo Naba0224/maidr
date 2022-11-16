2c77c55f0652fca304f9895c18bfc9c8e7e0d4b5
Naba0224 <naranbaatardev@gmail.com>
2022-11-16 17:27:30 +0800

    z

--- index.js
+++ index.js
@@ -6,15 +6,19 @@ app.use(express.json());
 app.post("/", (req, res) => {
   console.time("time");
   //ends with ni hedeer tugsuhiig ni haruulna
-  const endsWith = 9;
-  const theDigit = `${endsWith}`.length;
-  console.log(theDigit);
+  const endsWith = "45";
+
   fs.readFile("./4say300.log", "utf8", (err, file) => {
     const theFile = JSON.parse(file);
     const theNewArray = [];
-    const theDivider = 10 ** theDigit;
     for (let i = 0; i < theFile.length; i++) {
-      if (theFile[i] % theDivider === endsWith) {
+      const tempString = `${theFile[i]}`;
+      if (
+        tempString.substring(
+          tempString.length,
+          tempString.length - endsWith.length
+        ) === endsWith
+      ) {
         theNewArray.push(theFile[i]);
       }
     }
